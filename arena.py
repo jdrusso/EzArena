1b017f5907f2f5ef5f1ebb5c4a1ed78fbc185439
JD Russo <thejesusrambo@gmail.com>
2015-12-28 22:52:35 -0800

    FEATURE: OCR on all 3 card titles' text works.

--- arena.py
+++ arena.py
@@ -4,10 +4,11 @@
 # cv2
 from bs4 import BeautifulSoup as BS
 # from skimage.measure import structural_similarity as ssim
-from PIL import ImageGrab, Image, ImageOps
+from PIL import ImageGrab, Image, ImageOps, ImageEnhance
 import cv2, math, urllib
 import pytesser as pyt
 import uploader
+from difflib import SequenceMatcher as SM
 
 # This compares histograms by calculating Bhattacharyya distance. Seems to be the
 #   most accurate algorithm.
@@ -82,18 +83,85 @@ for rarity in RARITIES:
 
 # print(cards['beyond-great'])
 
-# Capture images of cards
-image = ImageGrab.grab(bbox=(385,390,610,440))
-image = ImageOps.invert(image)
-image.save("card1.jpg")
-url = uploader.upload("card1.jpg")
+coords = [(385,390,610,440),(670,390,895,440),(950,390,1175,440)]
 
-#Construct URL to raw image
-imgurl = url[:20] + 'extpics/' + url[20:]
+triplet = list()
+for i in range(3):
+    image = ImageGrab.grab(bbox=coords[i])
+    image.save("img/original_card%d.jpg" % i)
+    # image = ImageOps.invert(image)
+    # image.save("img/inverted_card%d.jpg" % i)
 
-# OCR card names from images
-textresult = urllib.urlopen(AWS_URL_START + imgurl + AWS_URL_END).read()
-print("OCR Result: %s" % textresult)
+    #contrast 1.5, brightness 3, contrast 10
+    # c = ImageEnhance.Contrast(image)
+    # image = c.enhance(1.5)
+    #
+    # b = ImageEnhance.Brightness(image)
+    # image = b.enhance(3)
+    #
+    # image = image.convert('L')
+    #
+    # c = ImageEnhance.Contrast(image)
+    # image = c.enhance(10)
+
+    # image = ImageEnhance.Contrast(ImageEnhance.Brightness(image).enhance(.15)).enhance(5.6).convert('1', dither=0)
+    # image = ImageOps.invert(image.convert('RGB')).convert('1', dither=0)
+    # image = ImageOps.invert(ImageOps.invert(image).point(lambda x: x*6 if x < 255/6 else 255))
+    image = ImageEnhance.Contrast(image.convert('L')).enhance(1.4).point(
+        lambda x: 0 if (x > 220 and 255) else 255, '1')
+
+    image.save("img/card%d.jpg" % i)
+    url = uploader.upload("img/card%d.jpg" % i)
+
+    #Construct URL to raw image
+    imgurl = url[:20] + 'extpics/' + url[20:]
+
+    # OCR card names from images
+    textresult = urllib.urlopen(AWS_URL_START + imgurl + AWS_URL_END).read()
+    print("OCR Result: %s" % textresult)
+    # print("Image URL: %s" % imgurl)
+
+    #Find which card the OCRed text corresponds to
+    bestMatch = 0
+
+    for rarity in RARITIES:
+        for card in cards[rarity]:
+            score = SM(None, str(textresult), str(card)).ratio()
+            if score > bestMatch:
+                bestMatch = score
+                cardMatch = card
+
+    print("Detected card was %s, confidence %f" % (cardMatch, score))
+    triplet.append(cardMatch)
+
+print("Available cards are %s, %s, and %s" % (triplet[0], triplet[1], triplet[2]))
+
+# # Capture images of cards
+# image = ImageGrab.grab(bbox=(385,390,610,440))
+#
+# image = ImageOps.invert(image)
+# image.save("card1.jpg")
+# url = uploader.upload("testimage.jpg")
+#
+# #Construct URL to raw image
+# imgurl = url[:20] + 'extpics/' + url[20:]
+#
+# # OCR card names from images
+# textresult = urllib.urlopen(AWS_URL_START + imgurl + AWS_URL_END).read()
+# print("OCR Result: %s" % textresult)
+# # print("Image URL: %s" % imgurl)
+#
+# #Find which card the OCRed text corresponds to
+# bestMatch = 0
+#
+# for rarity in RARITIES:
+#     for card in cards[rarity]:
+#         score = SM(None, textresult, card).ratio()
+#         if score > bestMatch:
+#             bestMatch = score
+#             cardMatch = card
+#
+# print("Detected card was %s" % cardMatch)
 
 # Draw colored overlay on screen over cards, i.e. green for best choice, red for worst
 
